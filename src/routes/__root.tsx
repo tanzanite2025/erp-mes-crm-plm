236e86fd0b9d65b826f99b011f1e6d1b6e1d5000
Tanzanite <brand.aftersales.service@gmail.com>
2026-05-09 16:03:34 +0800

    修复一个路由放错问题

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,7 @@ import { type QueryClient } from '@tanstack/react-query'
 import { createRootRouteWithContext } from '@tanstack/react-router'
 import { GeneralError } from '@/features/errors/general-error'
 import { NotFoundError } from '@/features/errors/not-found-error'
-import { RootRouteComponent } from '@/routes/root-route-component'
+import { RootRouteComponent } from '@/components/layout/root-route-component'
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient
